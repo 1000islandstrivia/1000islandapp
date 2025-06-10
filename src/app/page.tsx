72d2c85058936a3169e3f47818f5cc66973c2a54
Trent Herlehy <theramz2020@gmail.com>
2025-06-10 03:36:38 +0000

    use this image for the 1920 x 1080 background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen">
        <Image
-        src="https://placehold.co/1920x1080.png"
-        alt="Immersive island scenery background"
+        src="https://firebasestorage.googleapis.com/v0/b/storycraft-7050a.appspot.com/o/images%2Foriginal%2Fmap_of_thousand_islands.png_1720032762026?alt=media&token=9172e149-98a5-4e72-9d8f-5af2be13635d"
+        alt="Vintage map of Thousand Islands background"
         fill
         objectFit="cover"
         quality={85}
         className="fixed inset-0 z-[-1] opacity-40"
-        data-ai-hint="island scenery river"
+        data-ai-hint="vintage map"
       />
       <div className="fixed inset-0 bg-background/70 z-[-1]" /> {/* Overlay for readability */}
       <Header />
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,13 +5,13 @@ export default function RegisterPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center relative p-4">
        <Image
-        src="https://placehold.co/1920x1080.png"
-        alt="Immersive island scenery background"
+        src="https://firebasestorage.googleapis.com/v0/b/storycraft-7050a.appspot.com/o/images%2Foriginal%2Fmap_of_thousand_islands.png_1720032762026?alt=media&token=9172e149-98a5-4e72-9d8f-5af2be13635d"
+        alt="Vintage map of Thousand Islands background"
         fill
         objectFit="cover"
         quality={85}
         className="fixed inset-0 z-[-1] opacity-30"
-        data-ai-hint="river fog"
+        data-ai-hint="vintage map"
       />
       <div className="fixed inset-0 bg-background/80 z-[-1]" />
       <RegisterForm />
