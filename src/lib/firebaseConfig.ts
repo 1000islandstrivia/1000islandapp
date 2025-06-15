1acf6a1d50e9b236f44e0ce65936d208486f8f27
Trent Herlehy <theramz2020@gmail.com>
2025-06-15 22:40:51 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBULz1YfW73KytdHb5B9Q4mpO6Bsq2d

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -1,11 +1,10 @@
 // TODO: Replace with your actual Firebase project configuration
 // You can get this from your Firebase project settings
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID" // Optional
+  apiKey: "AIzaSyBULz1YfW73KytdHb5B9Q4mpO6Bsq2dKmk",
+  authDomain: "islands-riverrat-lore.firebaseapp.com",
+  projectId: "islands-riverrat-lore",
+  storageBucket: "islands-riverrat-lore.appspot.com",
+  messagingSenderId: "616143795008",
+  appId: "1:616143795008:web:8170a02065af4211ef80a5"
 };
