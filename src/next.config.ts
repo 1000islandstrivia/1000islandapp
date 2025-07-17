db245fa573ae1a7da428d41bdd7e661507411cbf
Trent Herlehy <theramz2020@gmail.com>
2025-07-17 00:48:40 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,40 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    // ignoreBuildErrors: true, // Removed
+  },
+  eslint: {
+    // ignoreDuringBuilds: true, // Removed
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  experimental: {
+  },
+  allowedDevOrigins: [
+      "6000-firebase-studio-1749524487777.cluster-ux5mmlia3zhhask7riihruxydo.cloudworkstations.dev"
+  ]
+};
+
+export default nextConfig;
